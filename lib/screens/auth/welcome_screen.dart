35cfc064662629192f6f088128c397c6fe6e9d05
Petar Cerović <quadrition@gmail.com>
2023-01-31 21:19:59 +0100

    Optimized layout on welcome screen

--- lib/screens/auth/welcome_screen.dart
+++ lib/screens/auth/welcome_screen.dart
@@ -30,51 +30,39 @@ class WelcomeScreen extends StatelessWidget {
             flex: 1,
             child: Padding(
               padding: const EdgeInsets.only(bottom: 20),
-              child: Row(
-                children: [
-                  const Expanded(
-                    flex: 5,
-                    child: SizedBox(),
-                  ),
-                  Expanded(
-                    flex: 90,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const Text(
-                          "Slušaj svoje omiljene knjige, bilo kada, bilo gde.",
-                          style: TextStyle(
-                              fontSize: 36, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        const Text(
-                          "Knjigapriča je prva online platforma za audio knjige na srpskom jeziku.",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        const SizedBox(
-                          height: 34,
-                        ),
-                        PrimaryButton(
-                            text: "Prijavi se / Kreiraj nalog",
-                            onPressed: () => _handleContinue(context)),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                        MyTextButton(
-                          text: 'Isprobaj besplatno',
-                          onPressed: () {},
-                        ),
-                      ],
+              child: FractionallySizedBox(
+                widthFactor: 0.9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Text(
+                      "Slušaj svoje omiljene knjige, bilo kada, bilo gde.",
+                      style:
+                          TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  const Expanded(
-                    flex: 5,
-                    child: SizedBox(),
-                  ),
-                ],
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const Text(
+                      "Knjigapriča je prva online platforma za audio knjige na srpskom jeziku.",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 34,
+                    ),
+                    PrimaryButton(
+                        text: "Prijavi se / Kreiraj nalog",
+                        onPressed: () => _handleContinue(context)),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    MyTextButton(
+                      text: 'Isprobaj besplatno',
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
